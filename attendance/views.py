132485fbb23382a87ef5163b400dbabe160ed2f4
adi271001 <67856422+adi271001@users.noreply.github.com>
2020-08-11 12:05:48 +0530

    Update views.py

--- attendance/views.py
+++ attendance/views.py
@@ -10,57 +10,55 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 # end rest test
 
-from .models import StudentPresence
-from main.models import Teacher, Student
+from .models import EmployeePresence
+from main.models import Admin, Employee
 
 import datetime
 
 
-class IsTeacher(permissions.BasePermission):
+class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-        teacher = Teacher.objects.filter(user=request.user)
-        if not teacher:
+       Admin = Admin.objects.filter(user=request.user)
+        if not Admin:
             return False
         return True
 
 
-class CurrentClassStudentsList(APIView):
+class CurrentEWmployeeList(APIView):
     def post(self, request, format=None):
-        current_class = request.user.teacher.has_class()
-        if not current_class:
+        current_class = request.user.Admin.has_class()
+        if not current_Employee:
             return Response(status=status.HTTP_204_NO_CONTENT)
-        current_class_students = [i.user for i in Student.objects.filter(reference_class=current_class.related_class)]
-        serializer = UserSerializer(current_class_students, many=True)
+        current_class_Employee = [i.user for i in Employee.objects.filter(reference_Employee=current_Employee.related_class)]
+        serializer = UserSerializer(current_class_employee, many=True)
         return Response(serializer.data)
-
-
-def teacher_only(func):
+def Admin_only(func):
     def wrapper(request):
         user = request.user
-        if Teacher.objects.filter(user=user):
+        if Admin.objects.filter(user=user):
             return func(request)
         elif request.user.is_superuser:   # redirecting to another function if the user is superuser or basically
-            return view_absents(request)    # school staff, principle or etc.
+            return view_absents(request)    # md, co-founders or etc.
         return Http404()
     return wrapper
 
 
 @api_view(['POST'])
-def current_class_students_list(request):
-    tracher = Teacher.objects.get(user=request.user)
-    current_class = Teacher.objects.get(user=request.user).has_class()
+def current_class_employee_list(request):
+    tracher = admin.objects.get(user=request.user)
+    current_class = admin.objects.get(user=request.user).has_class()
     if not current_class:
         return Response(status=status.HTTP_204_NO_CONTENT)
-    current_class_students = Student.objects.filter(reference_class=current_class.related_class)
-    serializer = UserSerializer(current_class_students, many=True)
+    current_class_employee = employee.objects.filter(reference_class=current_class.related_class)
+    serializer = UserSerializer(current_employee, many=True)
     return Response(serializer.data)
 
 
 @login_required
-@teacher_only
+@admin_only
 def attendance(request):
-    current_class = request.user.teacher.has_class()
-    prev_attendance = StudentPresence.objects.filter(class_lesson=current_class, date=datetime.date.today())
+    current_class = request.user.admin.has_class()
+    prev_attendance = EmployeePresence.objects.filter(class_work=current_class, date=datetime.date.today())
     if request.method == 'GET':
         if current_class:
             if prev_attendance:
@@ -68,7 +66,7 @@ def attendance(request):
             else:
                 prev_class = current_class.has_previous_class()
                 if prev_class:
-                    attendance_list = StudentPresence.objects.filter(class_lesson=prev_class[0],
+                    attendance_list = EmployeePresence.objects.filter(class_work=prev_class[0],
                                                                      date=datetime.date.today())
                     if attendance_list:
                         states = attendance_list
@@ -83,22 +81,22 @@ def attendance(request):
 
     elif request.method == 'POST':
         i = 0
-        for student in current_class.related_class.student_set.all():
+        for Employee in current_class.related_class.Employee_set.all():
             presence = True if request.POST.getlist('states[]')[i] == 'true' else False
             i += 1
             if prev_attendance:
-                obj = prev_attendance.filter(student=student)[0]
+                obj = prev_attendance.filter(Employee=Employee)[0]
                 if obj.present == presence:
                     continue
                 else:
                     obj.present = presence
                     obj.save()
             else:
-                obj = StudentPresence(student=student, present=presence, class_lesson=current_class,
+                obj = EmployeePresence(Employee=Employee, present=presence, class_work=current_class,
                                       date=datetime.date.today())
                 obj.save()
         return HttpResponse('ok')
 
 
-def view_absents(request):
-    return HttpResponseRedirect(reverse('management:show-absents'))
+def view_leaves(request):
+    return HttpResponseRedirect(reverse('management:show-leaves'))
