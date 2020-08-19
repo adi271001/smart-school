71093d144be281e70cd10c83785c357666a38ce7
adi271001 <67856422+adi271001@users.noreply.github.com>
2020-08-19 17:35:45 +0530

    Update models.py

--- attendance/models.py
+++ attendance/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from main.models import Student, ClassLesson
+from main.models import Employee, EmployeeWork
 
 
-class StudentPresence(models.Model):
-    student = models.OneToOneField(Student, models.CASCADE)
+class EmployeePresence(models.Model):
+    Employee = models.OneToOneField(Student, models.CASCADE)
     present = models.BooleanField(default=True)
-    class_lesson = models.ForeignKey(ClassLesson, on_delete=models.CASCADE)
+    Employee_work = models.ForeignKey(EmployeeWork, on_delete=models.CASCADE)
     date = models.DateField(auto_now=True)
     status = models.BooleanField(default=False)  # when administrators empty the absents it changes to True and won't
     # appear in the least again
